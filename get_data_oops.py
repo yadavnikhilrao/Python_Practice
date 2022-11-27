b386ec7811a1b48ccb5217e879047afb213e1a92
Nikhil Kumar <91002892+yadavnikhilrao@users.noreply.github.com>
2022-11-27 12:08:15 -0600

    Create get_data_oops.py

---
+++ get_data_oops.py
@@ -0,0 +1,11 @@
+class Student: 
+    '''Hello!'''
+    def __init__(self): 
+        self.name='Nikhil' 
+        self.age=40 
+        self.marks=80 
+
+    def talk(self): 
+        print("Hello I am :",self.name) 
+        print("My Age is:",self.age) 
+        print("My Marks are:",self.marks)
